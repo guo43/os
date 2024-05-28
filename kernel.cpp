2d39d200266a35716d087757ce5d7c5f722a35f6
ghc <2681374434@qq.com>
2024-05-28 21:39:23 +0800

    push gdt.cpp and types.h

--- kernel.cpp
+++ kernel.cpp
@@ -1,15 +1,27 @@
+#include "types.h"
+
 void printf(char* str)
 {
-	unsigned short * VideoMemory = (unsigned short*)0xb8000;
+	uint16_t* VideoMemory = (uint16_t*)0xb8000;
 	for(int i = 0; str[i] != '\0'; ++i)
 	{
 		VideoMemory[i] =  (VideoMemory[i] & 0xFF00) | str[i];
 	}
 }
 
-extern "C" constructor* start_ctors;
+typedef void (*constructor)();
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" constructor* start_ctors;
+extern "C" constructor* end_ctors;
+extern "C" void callConstructors()
+{
+	for(constructor * i = &start_ctors; i != end_ctors; i++)
+	{
+		(*i)();
+	}
+}
+
+extern "C" void kernelMain(void* multiboot_structure, uint32_t)
 {
 	printf("Hello World! --- https://baidu.com");
 	while(1);
