9a8d9f4c468ec0082c4dd663e60c07cf0d269ff0
ghc <2681374434@qq.com>
2024-06-04 21:57:12 +0800

    update interrupt

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ GCCPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-excep
 ASPARAMS = --32
 LDPARAMS = -melf_i386
 
-objects = loader.o gdt.o kernel.o
+objects = loader.o gdt.o kernel.o port.o interruptstubs.o interrupts.o
 
 %.o: %.cpp
 	gcc $(GCCPARAMS) -c -o $@ $<
--- kernel.cpp
+++ kernel.cpp
@@ -3,10 +3,41 @@
 
 void printf(char* str)
 {
-	uint16_t* VideoMemory = (uint16_t*)0xb8000;
+	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+	static uint8_t x = 0, y = 0;
+
 	for(int i = 0; str[i] != '\0'; ++i)
 	{
-		VideoMemory[i] =  (VideoMemory[i] & 0xFF00) | str[i];
+		switch(str[i])
+		{
+			case '\n':
+				y++;
+				x = 0;
+				break;
+			default:
+				VideoMemory[y*80+x] =  (VideoMemory[y*80+x] & 0xFF00) | str[i];
+				x++;
+		}
+
+		if(x >= 90)
+		{
+			y++;
+			x = 0;
+		};
+
+		if (y >= 25)
+		{
+			for(y = 0; y < 25; y++)
+			{
+				for (x = 0; x < 80; x++)
+				{
+					VideoMemory[y*80+x] =  (VideoMemory[y*80+x] & 0xFF00) | ' ';
+				}
+			}
+			x = 0;
+			y = 0;
+		}
 	}
 }
 
@@ -24,6 +55,7 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t)
 {
+	printf("Hello World! --- https://baidu.com\n");
 	printf("Hello World! --- https://baidu.com");
 	GlobalDescriptorTable gdt;
 	while(1);
